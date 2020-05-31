3e402a48418989c404bf9ae637dd55325164706f
lichaoqun <121708983@qq.com>
2020-05-31 18:09:01 +0800

    使用纹理作为环境光和材质

--- OpenGLES/OpenGL ES Learning/OpenGL ES Learning/demo09(环境光)/FragmentShader9.glsl
+++ OpenGLES/OpenGL ES Learning/OpenGL ES Learning/demo09(环境光)/FragmentShader9.glsl
@@ -51,3 +51,18 @@ void main(void) {
     vec3 result = ambient + diffuse + specular;
     gl_FragColor = vec4(result, 1.0);
 }
+
+// - 使用纹理做材质和环境光
+//precision mediump float;
+//varying vec2 TexCoords;
+//uniform sampler2D texture_diffuse;
+//uniform sampler2D texture_specular;
+//uniform sampler2D texture_height;
+//
+//void main()
+//{
+//    vec4 diffuse = texture2D(texture_diffuse, TexCoords);
+//    vec4 specular = texture2D(texture_specular, TexCoords);
+//    vec4 height = texture2D(texture_height, TexCoords);
+//    gl_FragColor = diffuse+specular+height;
+//}
